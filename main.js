40ae796e0f9ea4a99cc2709b93b4792e135c80c5
Jose Silva <js38902@gmail.com>
2023-12-20 19:58:57 -0500

    deleted misc

--- main.js
+++ main.js
@@ -39,41 +39,4 @@ function init() {
   });
 }
 
-// // create function to render svg.file
-// function makeSvg(answersObject) {
-//   const { characters, colorText, shape, logoColor } = answersObject;
-
-//   let shapeSvg;
-//   switch (shape) {
-//     case "circle":
-//       shapeSvg = `<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="${logoColor}" />`;
-//       break;
-//     case "triangle":
-//       shapeSvg = `<polygon points="50,10 90,90 10,90" stroke="black" stroke-width="3" fill="${logoColor}" />`;
-//       break;
-//     case "square":
-//       shapeSvg = `<rect width="80" height="80" x="10" y="10" stroke="black" stroke-width="3" fill="${logoColor}" />`;
-//       break;
-//     default:
-//       console.error("Invalid shape");
-//       return;
-//   }
-
-//   const finalSvgText = `<svg width="100" height="100">${shapeSvg}</svg>`;
-
-//   return finalSvgText;
-  // return "Here is your text from the makeSvg function"
-
-  // I might destructure the properties from the argument answersObject
-  // so const { characters, colorText, shape, logoColor } = answersObject
-
-  // you will need to use some logic to determine what shape to create based
-  // on the user input
-  // switch statement comes in handy here to handle the three shape cases
-
-  // use the params to create your svg element
-  // ....
-  // const finalSvgText = ...
-  // return finalSvgText
-// }
 init();
